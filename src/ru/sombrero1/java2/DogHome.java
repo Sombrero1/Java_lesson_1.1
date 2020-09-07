f6efbb0a424a688b97cc3886601315dd531d81e4
Sombrero1 <vovashevchenko1964@mail.ru>
2020-09-07 12:11:51 +0300

    SecondLesson(OOP)

--- src/ru/sombrero1/java2/DogHome.java
+++ src/ru/sombrero1/java2/DogHome.java
@@ -3,18 +3,23 @@ package ru.sombrero1.java2;
 import java.util.ArrayList;
 
 public class DogHome {
-    private ArrayList<Dog> dogs;
+    private  Dog [] dogs;
+    private int i=0;
     public DogHome() {
-        dogs= new ArrayList<Dog>();
+        dogs= new Dog[100];
 
 
     }
-    public void addDog(Dog dog){
-        System.out.println("Добавляем:" + dog);
-        dogs.add(dog);
+    public void addDog(Dog ... dog){
 
 
+        int j=0;
+        for(;j<dog.length;i++,j++){
+            System.out.println("Добавляем:" + dog[j]);
+            dogs[i]=dog[j];
 
+        }
+        System.out.println("====Добавили=====");
     }
 
 }
--- src/ru/sombrero1/java2/Programm.java
+++ src/ru/sombrero1/java2/Programm.java
@@ -5,9 +5,9 @@ public class Programm {
 
     public static void main(String[] args) {
         DogHome mirea=new DogHome();
-        mirea.addDog(new Dog("Putur", 15));
-        mirea.addDog(new Dog("Agr", 30));
-        mirea.addDog(new Dog("Putur", 40));
+        mirea.addDog(new Dog("Putur", 15),new Dog("Agr", 3));
+        mirea.addDog(new Dog("Putfdsfur", 15),new Dog("Agr", 3));
+
 
 
     }
